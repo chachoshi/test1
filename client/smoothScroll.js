78915fd0e6bb1e4ab93e62b79c2aa3e13280d18f
chachoshi <satoshi.isezaki@gmail.com>
2014-02-15 19:43:57 +0900

    shortcut function is added.

--- client/smoothScroll.js
+++ client/smoothScroll.js
@@ -17,3 +17,18 @@ $(document).ready(function() {
   });
 });
 
+$(document).on('keydown', null, 'ctrl+s', function() {
+  console.log('shortcut ctrl+s');
+  $('#btn_summary').trigger('click');
+});
+
+$(document).on('keydown', null, 'ctrl+b', function() {
+  console.log('shortcut ctrl+b');
+  $('#btn_breakdown').trigger('click');
+});
+
+$(document).on('keydown', null, 'ctrl+r', function() {
+  console.log('shortcut ctrl+r');
+  $('#btn_receipt').trigger('click');
+});
+
